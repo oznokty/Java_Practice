9b70b4fb95a3cd9beea79ca518dd1e4ed35ff3e8
Ozan Oktay <89248602+oznokty@users.noreply.github.com>
2023-06-01 17:35:05 +0300

    Sparse_Arrays.java

--- Sparse_Arrays.java
+++ Sparse_Arrays.java
@@ -12,17 +12,8 @@ import static java.util.stream.Collectors.toList;
 
 class Sparse_Arrays {
 
-    /*
-     * Complete the 'matchingStrings' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. STRING_ARRAY strings
-     *  2. STRING_ARRAY queries
-     */
 
     public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
-    // Write your code here
     
     String[] newstrings = new String[strings.size()];
         newstrings = strings.toArray(newstrings);
